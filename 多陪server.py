1cbcd875e414bed42aed7c261c4c8b94360fb07c
BountyHunterOka <434291091spd@gmail.com>
2025-12-12 19:40:30 +0800

    Update 多陪server.py

--- 多陪server.py
+++ 多陪server.py
@@ -187,7 +187,7 @@ def confirm_order(order_id):
                 break
             log(f"[抢单结果] {confirm_rep}")
             if '未满足' in confirm_rep:
-                time.sleep(1.5)
+                time.sleep(0.5)
                 log("等待中...继续尝试")
                 continue
             break
